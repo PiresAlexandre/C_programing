efdd0d23ed74763c004d44b13d18a158a0a1abee
AlexPires <pires.alexandre98@gmail.com>
2017-10-22 23:46:56 +0100

    Ficha2_ex1

---
+++ ficha2/Exercicio1.C/main.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Alexandre
+ *
+ * Created on 22 de Outubro de 2017, 23:44
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    puts("Ola mundo");
+
+    return (EXIT_SUCCESS);
+}
+
