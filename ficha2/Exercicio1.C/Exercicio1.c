c1e4e3ebfe9b35ce121944fa202e26e0c44f579a
AlexPires <pires.alexandre98@gmail.com>
2017-10-23 00:06:52 +0100

    Ficha2_Ex1

--- ficha2/Exercicio1.C/Exercicio1.c
+++ ficha2/Exercicio1.C/Exercicio1.c
@@ -18,7 +18,21 @@
  * 
  */
 int main(int argc, char** argv) {
-    puts("Ola mundo");
+    int numero1, numero2;
+    
+    printf("Introduza 1º numero: ");
+    scanf("%d", &numero1);
+    
+    printf("Introduza 2º numero: ");
+    scanf("%d", &numero2);
+    
+    if (numero1 > numero2) {
+        printf("O maior numero é: %d\n", numero1);
+    }else{
+        printf("O maior numero é: %d\n", numero2);
+    }
+    
+    
 
     return (EXIT_SUCCESS);
 }
--- ficha2/Exercicio1.C/nbproject/Makefile-Release.mk
+++ ficha2/Exercicio1.C/nbproject/Makefile-Release.mk
@@ -35,7 +35,7 @@ OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
 
 # Object Files
 OBJECTFILES= \
-	${OBJECTDIR}/main.o
+	${OBJECTDIR}/Exercicio1.o
 
 
 # C Compiler Flags
@@ -62,10 +62,10 @@ ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/exercicio1.c.exe: ${OBJECTFILES}
 	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
 	${LINK.c} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/exercicio1.c ${OBJECTFILES} ${LDLIBSOPTIONS}
 
-${OBJECTDIR}/main.o: main.c
+${OBJECTDIR}/Exercicio1.o: Exercicio1.c
 	${MKDIR} -p ${OBJECTDIR}
 	${RM} "$@.d"
-	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/main.o main.c
+	$(COMPILE.c) -O2 -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/Exercicio1.o Exercicio1.c
 
 # Subprojects
 .build-subprojects:
