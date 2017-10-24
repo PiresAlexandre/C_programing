24fb17fd2376e048c55df2b171019c416511f0b2
AlexPires <pires.alexandre98@gmail.com>
2017-10-24 17:42:25 +0100

    Exercicio13

---
+++ ficha2/Exercicio13.C/Exercicio13.c
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Alexandre
+ *
+ * Created on 24 de Outubro de 2017, 17:23
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    int temp_ideal;
+    float temp_atual, diferenca;
+    
+    puts("Qual é a temperatura atual?");
+    scanf("%f", &temp_atual);
+    
+    puts("Qual é a temperatura ideal?");
+    scanf("%d", &temp_ideal);
+    
+    if (temp_ideal < temp_atual){
+        diferenca = (float)temp_atual - temp_ideal;   
+        printf("Para reduzir a temperatura até a desejada necessitará de %.0fs\n", diferenca * 120);
+    }
+    
+    if (temp_ideal > temp_atual){
+        diferenca = (float)temp_ideal - temp_atual;   
+        printf("Para aumentar a temperatura até a desejada necessitará de %.0fs\n", diferenca * 180);
+    }
+    return (EXIT_SUCCESS);
+}
+
---
+++ ficha2/Exercicio13.C/build/Debug/Cygwin-Windows/Exercicio13.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin-Windows/Exercicio13.o: Exercicio13.c
