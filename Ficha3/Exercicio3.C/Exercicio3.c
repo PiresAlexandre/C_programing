ecf459dc39b65069bdc730eb72e2c116b7e393fa
AlexPires <pires.alexandre98@gmail.com>
2017-10-26 13:50:29 +0100

    Exercicio 1_2_3

---
+++ Ficha3/Exercicio3.C/Exercicio3.c
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Alexandre
+ *
+ * Created on 26 de Outubro de 2017, 12:18
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    int valor_inicial, valor_final;
+    
+    printf("Introduza o valor inicial: ");
+    scanf("%d", &valor_inicial);
+    
+    printf("Introduza o valor: ");
+    scanf("%d", &valor_final);
+    
+    
+    
+
+    return (EXIT_SUCCESS);
+}
+
