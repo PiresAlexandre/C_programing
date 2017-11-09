27dd6bf2ac20dacc03c09f963ca5b620d1dedacc
PiresAlexandre <pires.alexandre98@gmail.com>
2017-11-09 15:29:24 +0000

    Ex3

--- ficha5/Exercicio3.C/Exercicio3.c
+++ ficha5/Exercicio3.C/Exercicio3.c
@@ -13,12 +13,40 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#define LIMITE 10
 
 /*
  * 
  */
-int main(int argc, char** argv) {
 
+void CleanEntryBuffer() {
+    char ch;
+    while ((ch = getchar()) != '\n' && ch != EOF);
+}
+
+int main(int argc, char** argv) {
+    int x, i, arr[LIMITE], count;
+    char symbol, symbol2;
+
+    for (x = 0; x <= LIMITE; ++x) {
+        puts("Introduza um carater desejado: ");
+        scanf("%c", &symbol);
+    CleanEntryBuffer();
+        arr[x] = symbol;
+    }
+        
+        puts("Introduza segundo carater desejado: ");
+        scanf("%c", &symbol2);
+    CleanEntryBuffer();
+
+    for (i = 0; i <= LIMITE; ++i) {
+        if (symbol2 == arr[i]) {
+            ++count;
+        } 
+    }
+    
+    printf("O carater repete-se %d vezes\n", count);
+   
     return (EXIT_SUCCESS);
 }
 
