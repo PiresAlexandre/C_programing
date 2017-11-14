af35a5bfed94e9780062501f33738e200d653b6d
PiresAlexandre <pires.alexandre98@gmail.com>
2017-11-14 17:19:40 +0000

    restauro

--- ficha5/Exercicio5.C/source.c
+++ ficha5/Exercicio5.C/source.c
@@ -5,36 +5,36 @@
 void imprimirVetor(double array[]) {
     int x;
     
-    printf("\nO conteudo do vetor: ");
+    printf("\n\nO conteudo do vetor: \n");
     for (x = 0; x < LIMITE; ++x) {
-        printf(" %.2lf ", array[x]);
+        printf("%.2lf ", array[x]);
     }
 }
 
 void dobroVetor(double array[]) {
     int x;
     
-    puts("\nO dobro do vetor: ");
+    puts("\n\nDobro dos numeros no vetor: ");
     for (x = 0; x < LIMITE; ++x) {
-        printf(" %.2lf ", 2 * array[x]);
+        printf("%.2lf ", 2 * array[x]);
     }
 }
 
 void somaVetor(double array[]) {
     int x, soma = 0;
     
-    puts("\nA soma total do vetor: ");
+    puts("\n\nSoma dos numeros no vetor: ");
     for (x = 0; x < LIMITE; ++x) {
         soma += array[x];
     }
-    printf("%d\n ", soma);
+    printf("%d ", soma);
 }
 
 
 void mediaVetor(double array[]) {
     int x, soma = 0;
     
-    puts("\nA media total dos numeros introduzidos no vetor: ");
+    puts("\n\nMedia dos numeros introduzidos no vetor: ");
     for (x = 0; x < LIMITE; ++x) {
         soma += array[x];
     }
@@ -44,7 +44,7 @@ void mediaVetor(double array[]) {
 void maiorVetor(double array[]) {
     int x, num = 0;
     
-    puts("\nO maior elemento introduzido no vetor: ");
+    puts("\n\nMaior elemento introduzido no vetor: ");
     for (x = 0; x < LIMITE; ++x) {
         if (array[x] > num) {
             num = array[x];
@@ -56,7 +56,7 @@ void maiorVetor(double array[]) {
 void menorVetor(double array[]) {
     int x, num = array[0];
     
-    printf("\nO menor elemento introduzido no vetor e %d \n", num);
+    printf("\nMenor elemento introduzido no vetor e %d \n", num);
     for (x = 0; x < LIMITE; ++x) {
         if (array[x] < num) {
             num = array[x];
