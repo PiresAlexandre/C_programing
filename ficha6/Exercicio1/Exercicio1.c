60e084c866ceb0ef4fc00fd5efe163991c5ef2ab
PiresAlexandre <pires.alexandre98@gmail.com>
2017-11-21 15:59:43 +0000

    Ex1

---
+++ ficha6/Exercicio1/Exercicio1.c
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: Alexandre
+ *
+ * Created on 21 de Novembro de 2017, 15:50
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "utils.h"
+
+
+
+int main(int argc, char** argv) {
+    
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ ficha6/Exercicio1/build/Debug/Cygwin-Windows/Exercicio1.o.d
@@ -0,0 +1,3 @@
+build/Debug/Cygwin-Windows/Exercicio1.o: Exercicio1.c utils.h
+
+utils.h:
